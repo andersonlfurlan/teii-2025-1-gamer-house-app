9fb8ad2199c112c2d3019e69d0cd48570700218f
Anderson Furlan <alsfurlan@gmail.com>
2025-02-25 21:49:07 -0300

    Criadas páginas home, jogos e estúdios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'games',
+    loadChildren: () => import('./games/games.module').then( m => m.GamesPageModule)
+  },
+  {
+    path: 'studios',
+    loadChildren: () => import('./studios/studios.module').then( m => m.StudiosPageModule)
   }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
---
+++ src/app/games/games.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  templateUrl: './games.page.html',
+  styleUrls: ['./games.page.scss'],
+  standalone: false,
+})
+export class GamesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
